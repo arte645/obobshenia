b97d9c6c0f2d800e9e23dc1ce0cf6cfaf4d031c8
1 <1@DESKTOP-LSHE6BO>
2023-04-28 16:10:53 +0600

    Добавьте файлы проекта.

---
+++ obobshenia/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace obobshenia
+{
+    class massive<T>
+    {
+        private List<T> elements=new List<T>();
+
+        public List<T> Elements
+        {
+            get { return elements; }
+        }
+
+        public void Plus(T name) 
+        {
+            elements.Add(name);
+        }
+
+        public T Identify(int i)
+        {
+            try
+            {
+                return elements[i];
+            }
+            catch
+            {
+                Console.WriteLine("Элемент с данным индексом отсутствует, введите новый \n"); // типа ввели
+                return Identify(i-1);
+            }
+        }
+
+        public void Delete(T element) 
+        {
+            elements.Remove(element);
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            massive<int> intMassive = new massive<int>();
+
+            intMassive.Plus(1);
+            intMassive.Plus(2);
+            intMassive.Plus(3);
+
+            foreach (var bup in intMassive.Elements)
+            {
+                Console.WriteLine(bup+"\n");
+            }
+
+            Console.WriteLine(intMassive.Identify(3));
+
+            intMassive.Delete(1);
+            intMassive.Delete(2);
+            intMassive.Delete(3);
+
+            foreach (var bup in intMassive.Elements)
+            {
+                Console.WriteLine(bup);
+            }
+
+            if (intMassive.Elements.Count == 0)
+            {
+                Console.WriteLine("Список пуст");
+            }
+        }
+    }
+}
